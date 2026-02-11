22b19fc61b51d359ccd34877a42b3638ffd4deb7
chighwood <hig23003@byui.edu>
2026-02-11 17:31:10 -0600

    Car Database Fix / Main Page

--- Med-Vehicle/Models/CarInfo.cs
+++ Med-Vehicle/Models/CarInfo.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using global:: MongoDB.Bson;
 using global:: MongoDB.Bson.Serialization.Attributes;
 
+[BsonIgnoreExtraElements]
 public class Car
 {
     [BsonId]
@@ -36,4 +37,9 @@ public class Car
     public string? Description { get; set; }
 
     public string? ImageFileName { get; set; }
+
+    // Link to the user who created/owns this car (stored as ObjectId in Mongo)
+    [BsonElement("UserId")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? UserId { get; set; }
 }
